9a1877051718ee6f2a13e1bd90f1722cb28c6cc5
zhangzhengyang <zhangzhengyang27@aliyun.com>
2022-08-03 14:43:39 +0800

    仿打字机效果

--- 20-typewritter-effect/index.js
+++ 20-typewritter-effect/index.js
@@ -1,6 +1,4 @@
-// 获取显示文字的span元素
 const textEl = document.querySelector("#text");
-// 获取并解析要展示的文本数组
 const texts = JSON.parse(textEl.getAttribute("data-text"));
 
 // 当前显示文本数组中的第几个
@@ -15,49 +13,48 @@ let start = null;
 // 是否为删除动画
 let isDeleting = false;
 
-// 动画回调函数
+// 动画回调函数  time是一直增加的
 function type(time) {
-  window.requestAnimationFrame(type);
-  // 初始化开始时间
-  if (!start) start = time;
-  // 获取时间间隔
-  let progress = time - start;
-  // 每隔一定的时间，打印出一个新的字符
-  if (progress > delta) {
-    // 获取完整的字符
-    let text = texts[index];
-    // 如果是打字效果
-    if (!isDeleting) {
-      // 给展示文字的span新增一个字符，使用innerHTML来替换，charIndex自增1，然后返回新的字符串子串
-      textEl.innerHTML = text.slice(0, ++charIndex);
-      // 每个字符打印出来的速度不一样，模仿人工打字的速度
-      delta = 500 - Math.random() * 400;
-    } else {
-      // 如果是删除效果，则把文字一个一个减掉
-      textEl.innerHTML = text.slice(0, charIndex--);
-    }
-    // 把star更新为当前时间，进行下一个周期
-    start = time;
+    window.requestAnimationFrame(type);
 
-    // 如果文字已经全部打印完毕
-    if (charIndex === text.length) {
-      // 下次开始删除文字
-      isDeleting = true;
-      // 删除文字的间隔为200毫秒
-      delta = 200;
-      // 额外等待1.2秒后再删除
-      start = time + 1200;
-    }
+    // 初始化开始时间
+    if (!start) start = time;
 
-    // 如果文字删除完毕
-    if (charIndex < 0) {
-      isDeleting = false;
-      // 额外增加200毫秒延迟
-      start = time + 200;
-      // 把index移动到下一个文本，并且在文本数组元素个数中循环
-      index = ++index % texts.length;
+    // 获取时间间隔
+    let progress = time - start;
+    console.log(time)
+    // 每隔一定的时间，打印出一个新的字符
+    if (progress > delta) {
+        // 获取完整的字符
+        let text = texts[index];
+        // 如果是打字效果
+        if (!isDeleting) {
+            // 给展示文字的span新增一个字符，使用innerHTML来替换，charIndex自增1，然后返回新的字符串子串
+            textEl.innerHTML = text.slice(0, ++charIndex);
+            // 每个字符打印出来的速度不一样，模仿人工打字的速度
+            delta = 500 - Math.random() * 400;
+        } else {
+            // 如果是删除效果，则把文字一个一个减掉
+            textEl.innerHTML = text.slice(0, charIndex--);
+        }
+        // 把star更新为当前时间，进行下一个周期
+        start = time;
+
+        // 如果文字已经全部打印完毕
+        if (charIndex === text.length) {
+            isDeleting = true;
+            delta = 200;
+            // 额外等待1.2秒后再删除
+            start = time + 1200;
+        }
+
+        // 如果文字删除完毕
+        if (charIndex < 0) {
+            isDeleting = false;
+            start = time + 200;
+            index = ++index % texts.length;
+        }
     }
-  }
 }
 
 window.requestAnimationFrame(type);
